8c12d7f5d40c78c65afd20239926904dd6706110
Shakibul Islam <8080.shakib@gmail.com>
2025-02-02 14:17:42 +0600

    Refactor ticket card styles and update rendering logic for improved performance

--- js/allTickets.js
+++ js/allTickets.js
@@ -7,18 +7,19 @@ function renderTickets() {
     fetch("https://raffle-draw-server-mu.vercel.app/api/v1/tickets")
         .then((res) => res.json())
         .then((tickets) => {
-            tickets.forEach((ticket) => {
+            loader.classList.remove("loader--open");
+            for (let i = tickets.length - 1; i >= 0; i--) {
+                const ticket = tickets[i];
                 const clone = template.content.cloneNode(true);
                 clone.querySelector(".ticketCard__id").textContent = `ID: ${ticket.id}`;
                 clone.querySelector(".ticketCard__username").textContent = `${ticket.username}`;
                 clone.querySelector(".ticketCard__price").textContent = `$${ticket.price}`;
                 clone.querySelector(".ticketCard__date").textContent = `${formatDate(ticket.updateDate)}`;
                 // Apply random transform to card
-                const rotate = getRandomValue(30) - 15; // Random value between -5 and 5 
+                const rotate = getRandomValue(30) - 15;
                 clone.querySelector(".ticketCard").style.transform = `rotate(${rotate}deg)`;
                 grid.appendChild(clone);
-                loader.classList.remove("loader--open");
-            });
+            }
         }).catch((err) => {
             console.error(err);
         });
